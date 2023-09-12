a396070ad87ce2663d94a69789f20a31272ffb7e
Dipshikhasarangpure <144772917+Dipshikhasarangpure@users.noreply.github.com>
2023-09-12 16:04:02 +0530

    Add files via upload
    java codes

---
+++ AdditionPgm.java
@@ -0,0 +1,17 @@
+public class AdditionPgm
+{
+public static void main(String args[])
+{
+System.out.println("My first program is Java");
+int a=88, b=90;
+int add, sub, mul, div;
+add = a+b;
+sub = a-b;
+mul = a*b; 
+div = a/b;
+System.out.println("Addition :"+ add);
+System.out.println("substration :"+ sub);
+System.out.println("Multiplication :"+ mul);
+System.out.println("Division :"+ div);
+}
+}
---
+++ IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse{
+
+public static void main(String args[]){
+
+int a = 23;
+if (a%2 == 0){
+	System.out.println(a + " is an even number");
+}else{
+	System.out.println(a + " is an odd number");
+   }
+}
+}
